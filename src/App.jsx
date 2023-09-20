3719617bd2bc03686c10c0ee6ebabf3d4c9afa69
Homsek <mihgfgbj@gmail.com>
2023-09-20 15:34:59 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+
+import React from "react";
+// Задание 7
+// function App() {
+//   return <div >
+//     <div class="text">text1</div>
+//     <div class="text">text2</div>
+//     </div>;
+// }
+
+// Задание 8
+// function App() {
+//   return (
+//     <ul>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//       <li>aaa</li>
+//     </ul>
+//   );
+// }
+
+// Задание 9
+// function App() {
+// 	return(
+// 		<ul>
+// 			<li>text1</li>
+// 			<li>text2</li>
+// 			<li>text3</li>
+// 		</ul>
+//     );
+// }
+
+// Задание 10
+// Способ1
+// function App() {
+//   return (
+//     <>
+//       <ul>
+//         <li>text1</li>
+//         <li>text2</li>
+//         <li>text3</li>
+//       </ul>
+//       <ul>
+//         <li>text1</li>
+//         <li>text2</li>
+//         <li>text3</li>
+//       </ul>
+//     </>
+//   );
+// }
+// Способ2
+// function App() {
+//   return (
+//     <div>
+//       <ul>
+//         <li>text1</li>
+//         <li>text2</li>
+//         <li>text3</li>
+//       </ul>
+//       <ul>
+//         <li>text1</li>
+//         <li>text2</li>
+//         <li>text3</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// Задание 11
+// function App() {
+// 	return <input/>;
+// }
+
+// Задание 12
+// function App() {
+// 	return <>
+//   <input /><input /><input />
+//   </>;
+// }
+
+// Задание 13
+// function App() {
+// 	const str1 = 'text1';
+// 	const str2 = 'text1';
+	
+// 	return <div>
+// 		<p>{str1}</p>
+// 		<p>{str2}</p>
+// 	</div>;
+// }
+
+// Задание 14
+// function App() {
+// 	const name = 'user';
+// 	const age  = '30';
+	
+// 	return <div>
+// 		name: {name}
+// 		age:  {age}
+// 	</div>;
+// }
+
+export default App;
